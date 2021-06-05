b3ac1efe84f76d34c52e8cb1d031062ad1f52fbc
Viacheslav Chuvalskyi <slavchuval@gmail.com>
2021-06-05 22:12:49 +0300

    Add some classes, state, functions

--- src/components/ToDoItem/ToDoItem.js
+++ src/components/ToDoItem/ToDoItem.js
@@ -1,20 +1,74 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import './ToDoItem.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
-const CreateToDoItem = ({label} )=>{
+export default class extends Component{
 
-    return (
-        <div className='todo-list-item-block'>
-            <span className='todo-list-item'> {label}</span>
-            <div className='todo-list-item-icons'>
-                <FontAwesomeIcon icon={['fas', 'trash-alt']}  className='toDoitemIcons'/>
-                <FontAwesomeIcon icon={['fas', 'minus-square']} className='toDoitemIcons'/>
+    state = {
+        done: false,
+        important: false
+    };
+
+    onClickLabel = () => {
+
+        let done = !this.state.done;
+
+        this.setState({
+            done
+        });
+
+    };
+
+    onClickImportant = () => {
+
+        let important = !this.state.important;
+
+        this.setState({
+            important
+        });
+
+    };
+
+    onClickDelete = () => {
+
+    };
+
+    render()
+    {
+        const {label} = this.props;
+        const {done,important}  = this.state;
+
+        let classNames = 'todo-list-item';
+
+        if(done){
+            classNames += ' todo-list-item-done';
+        }
+        if(important){
+            classNames += ' todo-list-item-important';
+        }
+
+        return (
+            <div className='todo-list-item-block'>
+                <span
+                    className = {classNames}
+                    onClick={this.onClickLabel}>
+                    {label}
+                </span>
+                <div className='todo-list-item-icons'>
+                    <FontAwesomeIcon
+                        icon={['fas', 'trash-alt']}
+                        className='toDoitemIcons'
+                        onClick = {this.onClickDelete}
+                    />
+
+                    <FontAwesomeIcon
+                        icon={['fas', 'exclamation-circle']}
+                        className='toDoitemIcons'
+                        onClick = {this.onClickImportant}
+                    />
+                </div>
             </div>
-        </div>
-
-    )
+        )
+    }
 };
-
-export default CreateToDoItem;
